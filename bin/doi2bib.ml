d457f58ddbe43c2b0742a8db5b0d03a4c08c4ad9
Marcello Seri <marcello.seri@gmail.com>
2021-03-18 22:07:35 +0100

    Small cleanup
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- bin/doi2bib.ml
+++ bin/doi2bib.ml
@@ -189,51 +189,47 @@ let doi2bib id =
       err
       @@ Printf.sprintf
            "Error: unable to find a DOI entry for %s.\n"
-           (string_of_id @@ parse_id id)
+           id
     | exception Entry_not_found ->
       err
       @@ Printf.sprintf
-           "Error: unable to find any bibtex entry for %s.\nCheck the ID before trying \
-            again.\n"
-           (string_of_id @@ parse_id id)
+           "Error: unable to find any bibtex entry for %s.\n\
+            Check the ID before trying again.\n"
+           id
     | exception Failure s -> err @@ Printf.sprintf "Unexpected error. %s\n" s
     | exception Bad_gateway ->
       err
       @@ Printf.sprintf
-           "Remote server error: wait some time and try again.\nThis error tends to \
-            happen when the remote servers are busy."
+           "Remote server error: wait some time and try again.\n\
+            This error tends to happen when the remote servers are busy."
     | exception Parse_error id ->
       err
       @@ Printf.sprintf
-           "Error: unable to parse ID: '%s'.\nYou can force me to consider it by \
-            prepending 'doi:', 'arxiv:' or 'PMC' as appropriate."
+           "Error: unable to parse ID: '%s'.\n\
+            You can force me to consider it by prepending 'doi:', 'arxiv:' or 'PMC' as \
+            appropriate."
            id)
-
-
-let id =
-  let open Cmdliner in
-  let doc =
-    "A DOI, an arXiv ID or a PubMed ID. The tool tries to automatically infer what kind \
-     of ID you are using. You can force the cli to lookup a DOI by using the form \
-     'doi:ID' or an arXiv ID by using the form 'arXiv:ID'.\n\
-     PubMed IDs always start with 'PMC'."
-  in
-  Arg.(value & pos 0 (some string) None & info ~docv:"ID" ~doc [])
-
-
-let doi2bib_t = Cmdliner.Term.(ret (const doi2bib $ id))
-
-let info =
-  let open Cmdliner in
-  let doc =
-    "A little CLI tool to get the bibtex entry for a given DOI, arXiv or PubMed ID."
-  in
-  let man =
-    [ `S Manpage.s_bugs; `P "Report bugs to https://github.com/mseri/doi2bib/issues" ]
-  in
-  Term.info "doi2bib" ~version:"%‌%VERSION%%" ~doc ~exits:Term.default_exits ~man
 
 
 let () =
   let open Cmdliner in
+  let id =
+    let doc =
+      "A DOI, an arXiv ID or a PubMed ID. The tool tries to automatically infer what \
+       kind of ID you are using. You can force the cli to lookup a DOI by using the form \
+       'doi:ID' or an arXiv ID by using the form 'arXiv:ID'.\n\
+       PubMed IDs always start with 'PMC'."
+    in
+    Arg.(value & pos 0 (some string) None & info ~docv:"ID" ~doc [])
+  in
+  let doi2bib_t = Term.(ret (const doi2bib $ id)) in
+  let info =
+    let doc =
+      "A little CLI tool to get the bibtex entry for a given DOI, arXiv or PubMed ID."
+    in
+    let man =
+      [ `S Manpage.s_bugs; `P "Report bugs to https://github.com/mseri/doi2bib/issues" ]
+    in
+    Term.info "doi2bib" ~version:"%‌%VERSION%%" ~doc ~exits:Term.default_exits ~man
+  in
   Term.exit @@ Term.eval (doi2bib_t, info)
